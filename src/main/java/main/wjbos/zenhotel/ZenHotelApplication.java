8b29ed058882279e273cd5f711235a12c84c2af8
WJ-Boshoff <willemwors15@gmail.com>
2024-08-26 22:17:21 +0200

    Initial commit

---
+++ src/main/java/main/wjbos/zenhotel/ZenHotelApplication.java
@@ -0,0 +1,13 @@
+package main.wjbos.zenhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/wjbos/zenhotel/ZenHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package main.wjbos.zenhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
