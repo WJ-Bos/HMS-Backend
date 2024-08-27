1bad1b7c4b9dcd8d7ae742f6e9130ee66d7fff93
WJ-Boshoff <willemwors15@gmail.com>
2024-08-27 18:50:30 +0200

    Finished room Entity

--- src/main/java/main/wjbos/zenhotel/Entity/Room.java
+++ src/main/java/main/wjbos/zenhotel/Entity/Room.java
@@ -3,6 +3,11 @@ package main.wjbos.zenhotel.Entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.awt.print.Book;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "rooms")
@@ -11,4 +16,24 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    private List<Booking> bookings = new ArrayList<>();
+
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice=" + roomPrice +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                '}';
+    }
 }
