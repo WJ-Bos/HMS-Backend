c37cb334c8edb5453c482163c5f1abe3977aacf2
WJ-Boshoff <willemwors15@gmail.com>
2024-09-08 21:33:51 +0200

    Removed static method and comments

--- src/main/java/main/wjbos/zenhotel/Security/JWTAuthFilter.java
+++ src/main/java/main/wjbos/zenhotel/Security/JWTAuthFilter.java
@@ -29,43 +29,28 @@ public class JWTAuthFilter extends OncePerRequestFilter { //first line of Securi
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // Get the Authorization header from the request
         final String authHeader = request.getHeader("Authorization");
-        // Extract the JWT token from the Authorization header
         final String jwtToken;
-        // Extract the email from the JWT token
         final String email;
 
-        // If the Authorization header is null or blank, skip authentication
         if(authHeader == null || authHeader.isBlank()){
             filterChain.doFilter(request,response);
             return;
         }
 
-        // Extract the JWT token from the Authorization header
         jwtToken = authHeader.substring(7);
-        // Extract the email from the JWT token
         email = jwtUtils.extractUsername(jwtToken);
 
-        // If the email is valid and the security context is not yet set, authenticate the user
         if(email != null && SecurityContextHolder.getContext().getAuthentication() == null){
-            // Load the user from the database
             UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-            // Check if the JWT token is valid
             if(jwtUtils.isValidToken(jwtToken,userDetails)){
-                // Create a new security context
                 SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
-                // Create a new authentication token
                 UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
-                // Set the details of the authentication token
                 token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                // Set the authentication token in the security context
                 securityContext.setAuthentication(token);
-                // Set the security context in the security holder
                 SecurityContextHolder.setContext(securityContext);
             }
         }
-        // Continue with the filter chain
         filterChain.doFilter(request,response);
     }
 }
