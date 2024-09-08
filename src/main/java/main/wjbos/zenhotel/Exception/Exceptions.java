999afc0557fea15c97cb9061df4de9797833b8c1
WJ-Boshoff <willemwors15@gmail.com>
2024-09-08 16:15:40 +0200

    Custom exception class

---
+++ src/main/java/main/wjbos/zenhotel/Exception/Exceptions.java
@@ -0,0 +1,11 @@
+package main.wjbos.zenhotel.Exception;
+
+/**
+ * Class used to throw exceptions
+ */
+public class Exceptions extends RuntimeException{
+
+    public Exceptions(String message) {
+        super(message);
+    }
+}
